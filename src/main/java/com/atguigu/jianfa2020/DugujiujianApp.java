500228ff54b6a162aee24dc800d7892d8a6423f6
yuebuqun <m1607890768@gmail.com>
2020-01-14 13:18:28 +0800

    辟邪剑法

--- src/main/java/com/atguigu/jianfa2020/DugujiujianApp.java
+++ src/main/java/com/atguigu/jianfa2020/DugujiujianApp.java
@@ -2,6 +2,10 @@ package com.atguigu.jianfa2020;
 
 public class DugujiujianApp {
     public static void main(String[] args) {
-        System.out.println("hello    独孤九剑!");
+        System.out.println("hello    辟邪剑法!");
+        System.out.println("hello    辟邪剑法!");
+        System.out.println("hello    辟邪剑法!");
+        System.out.println("hello    辟邪剑法!");
+        System.out.println("hello    辟邪剑法!");
     }
 }
